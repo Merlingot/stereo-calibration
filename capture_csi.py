2d370b95396f51bddc609098e2ab7959f4344518
Merlingot <merlingot1@gmail.com>
2021-02-19 10:11:33 -0500

    ménage

--- capture_csi.py
+++ capture_csi.py
@@ -1,13 +1,11 @@
-##############################################################################################################
-# Projet : Integration cours initiation                                                                      #
-# Description Script : Effectuer une capture synchrone de deux camera NANO                                       #
-# Date : 29 aout 2020                                                                                        #
-# Auteur : Carl Beaulieu                                                                                     #
-##############################################################################################################
+################################################################################
+# Projet : Integration cours initiation
+# Description Script : Effectuer une capture synchrone de deux cameras
+# Date : 29 aout 2020
+# Auteur : Carl Beaulieu
+################################################################################
 
-import cv2
-# import nanocamera as nano
-
+import cv2 as cv
 from modules.util import clean_folders
 
 # Variables textuel
@@ -24,12 +22,8 @@ clean_folders([folder])
 
 
 # Déclaration du périphérique de capture
-# nano.Camera()
-# cap_left = nano.Camera(device_id=0, flip=2, width=800,height=480,fps=30)
-# cap_right = nano.Camera(device_id=1, flip=2, width=800,height=480,fps=30)
-
-cap_left = cv2.videoCapture(0)
-cap_left = cv2.videoCapture(1)
+cap_left = cv.videoCapture(0)
+cap_left = cv.videoCapture(1)
 
 while(True):
     if capture_taken==0:
@@ -45,37 +39,37 @@ while(True):
 
 
     #Transformation de l'image en nuances de gris
-    left_frame = cv2.cvtColor(left_frame_color, cv2.COLOR_BGR2GRAY)
-    right_frame = cv2.cvtColor(right_frame_color, cv2.COLOR_BGR2GRAY)
+    left_frame = cv.cvtColor(left_frame_color, cv.COLOR_BGR2GRAY)
+    right_frame = cv.cvtColor(right_frame_color, cv.COLOR_BGR2GRAY)
 
     # Enregistrement des deux frames sans écritures
-    if cv2.waitKey(1) == ord('c'):
+    if cv.waitKey(1) == ord('c'):
         if capture_taken<capture_qty:
             capture_taken = capture_taken + 1
             text_progress=""
             filename_l="{}left{:03d}".format(folder,capture_taken) + ".jpg"
             filename_r="{}right{:03d}".format(folder,capture_taken) + ".jpg"
-            cv2.imwrite(filename_l, left_frame)
-            cv2.imwrite(filename_r, right_frame)
+            cv.imwrite(filename_l, left_frame)
+            cv.imwrite(filename_r, right_frame)
 
     # Affichage du contenu
-    left_view = left_frame_color #cv2.resize(left_frame_color,(480,270))
-    right_view = right_frame_color #cv2.resize(right_frame_color,(480,270))
+    left_view = left_frame_color #cv.resize(left_frame_color,(480,270))
+    right_view = right_frame_color #cv.resize(right_frame_color,(480,270))
 
 
-    left_view=cv2.flip(left_view,1)
-    right_view=cv2.flip(right_view,1)
+    left_view=cv.flip(left_view,1)
+    right_view=cv.flip(right_view,1)
 
 
-    cv2.putText(left_view, text_progress, (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-    cv2.putText(right_view, text_progress, (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-    cv2.imshow(text_title + ' Camera Gauche',left_view)
-    cv2.imshow(text_title + ' Camera Droite',right_view)
+    cv.putText(left_view, text_progress, (60, 60), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    cv.putText(right_view, text_progress, (60, 60), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    cv.imshow(text_title + ' Camera Gauche',left_view)
+    cv.imshow(text_title + ' Camera Droite',right_view)
 
-    if cv2.waitKey(1) == 27:
+    if cv.waitKey(1) == 27:
         break
 
 # On relache la connexion
 cap_left.release()
 cap_right.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
