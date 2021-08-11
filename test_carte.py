58984ca588f9b1ee4dd84fce91cf69b6fa8cf854
Merlingot <merlingot1@gmail.com>
2021-08-11 17:55:36 -0400

    ok

--- test_carte.py
+++ test_carte.py
@@ -16,22 +16,23 @@ cam1,cam2=get_cameras(left_xml, right_xml, alpha=0)
 cam1.set_images(left)
 cam2.set_images(right)
 
-cloud, mask, depth_map = calcul_mesh(cam1.rectified, cam2.rectified, cam1.Q, downscale=2, sigma_wls=1.5, fbs_spatial=8.0, bilateral_on=False)
+cloud, mask, depth_map = calcul_mesh(cam1.rectified, cam2.rectified, cam1.Q, downscale=2, sigma_wls=1.5, fbs_spatial=8.0, bilateral_on=True)
+# np.savetxt('depth_map.txt', depth_map)
 # ------------------------------------------------------------------------------
 
 # Montrer la carte de profondeur -----------------------------------------------
 depth_map=cloud[:,:,2]*mask
 fig, ax=plt.subplots()
 a = plt.imshow(depth_map)
-a.set_clim(0,35)
+# a.set_clim(0,35) #Pour mettre des limites au colorbar. Peut aider à mieux visualiser.
 plt.colorbar()
 plt.show()
 # ------------------------------------------------------------------------------
 
 # Sauvegarder le mesh ----------------------------------------------------------
-colors = cv.cvtColor(cam1.rectified, cv.COLOR_BGR2RGB)
-colors_valides = colors[mask.astype(bool)]
-points_valides=cloud[mask.astype(bool)]
-out_fn = 'output/3dpoints/{}.ply'.format('my_mesh')
-write_ply(out_fn, points_valides, colors_valides)
+# colors = cv.cvtColor(cam1.rectified, cv.COLOR_BGR2RGB)
+# colors_valides = colors[mask.astype(bool)]
+# points_valides=cloud[mask.astype(bool)]
+# out_fn = 'output/3dpoints/{}.ply'.format('my_mesh')
+# write_ply(out_fn, points_valides, colors_valides)
 # ------------------------------------------------------------------------------
